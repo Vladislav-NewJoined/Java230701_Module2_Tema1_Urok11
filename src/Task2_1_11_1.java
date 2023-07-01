61408e18e8e67ee38385beed7d9878f704e3c4cf
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-01 19:43:25 +0300

    Initial commit

--- src/Task2_1_11_1.java
+++ src/Task2_1_11_1.java
@@ -1,5 +1,6 @@
-public class Main {
+public class Task2_1_11_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tРазбор домашнего задания 1.11\n\nРешение: ");
+        System.out.println("Ознакомлен.");
     }
 }
